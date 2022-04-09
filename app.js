df740cb9dd871de1d9f516a3aecc148f5a02752f
Thy Doan <doanthienthy91@gmail.com>
2022-04-09 19:11:37 +0700

    Chuyển đổi ứng dụng sang Handlebars

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -18,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Nor Found'});
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
 app.listen(3000);
