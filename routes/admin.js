1b9902a94be9c8292c455d3e677a098a08898def
Thy Doan <doanthienthy91@gmail.com>
2022-04-10 09:04:56 +0700

    Thêm ID sản phẩm vào đường dẫn

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// /admin/add-product ==> GET
-router.get('/add-product', productsController.getAddProduct);
-
-// /admin/add-product ==> POST
-router.post('/add-product', productsController.postAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
