5515124bfc3883db281551438b19ac93574b1619
Thy Doan <doanthienthy91@gmail.com>
2022-04-10 10:00:15 +0700

    Hiển thị trang: Chi tiết Sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const proId= req.params.productId;
   Product.findById(proId, product => {
-    console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
-  res.redirect('/');
 }
 
 exports.getIndex = (req, res, next) => {
