30be43d65bdb6b3329186809b4d72c6c7be0c2f1
Thy Doan <doanthienthy91@gmail.com>
2022-04-11 12:35:34 +0700

    Lấy dữ liệu

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: '123456'
 });
 
 module.exports = pool.promise();
