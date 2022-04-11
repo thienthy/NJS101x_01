49ec7f91eb87b75b179cafa08fd33c0a79bd7b73
Thy Doan <doanthienthy91@gmail.com>
2022-04-11 22:54:49 +0700

    Thêm sản phẩm mới vào giỏ hàng

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -44,25 +43,25 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-    // .sync({ force: true })
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-        // console.log(result);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Max', email: 'test@test.com' });
-        }
-        return user;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(user => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
+//   .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
