eb0b8a4ec5d71577daaa4e943dd91811faedfe01
Thy Doan <doanthienthy91@gmail.com>
2022-04-11 16:01:04 +0700

    Kết nối với Cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '123456'
+const sequelize = new Sequelize('node-complete', 'root', '123456', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
