bb13ba37b0a94b8f20ef18b939be43f6ad091e76
Thy Doan <doanthienthy91@gmail.com>
2022-04-11 14:27:02 +0700

    Thực hành Tìm nạp sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,9 +2,10 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
