5900f374fce818a4d76d9592c71ec1a0a794c47e
Thy Doan <doanthienthy91@gmail.com>
2022-04-11 23:11:52 +0700

    Xóa các mặt hàng liên quan & Xóa sản phẩm giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -105,10 +105,18 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      const product = products[0]
+      return product.cartItem.destroy();
+    })
+    .then(ressult => {
+      res.redirect('/cart');
+    })
 };
 
 exports.getOrders = (req, res, next) => {
