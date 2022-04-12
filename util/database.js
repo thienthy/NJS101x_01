adf7719263629e024984852d64ca76e1ca869cc7
Thy Doan <doanthienthy91@gmail.com>
2022-04-12 20:59:27 +0700

    Cài đặt trình điều khiển MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '123456', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://thienthy:thienthy91@cluster0.ouuxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+        .then(client => {
+            console.log('Connect!');
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
