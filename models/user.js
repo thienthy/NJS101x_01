95b107b84e1e6e66eb77a5f26edd10666f25d58d
Thy Doan <doanthienthy91@gmail.com>
2022-04-14 06:34:45 +0700

    Thêm dữ liệu thứ tự quan hệ

--- models/user.js
+++ models/user.js
@@ -80,9 +80,17 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db
-            .collection('orders')
-            .insertOne(this.cart)
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.name
+                    }
+                };
+                return db.collection('orders').insertOne(order);
+            })        
             .then(result => {
                 this.cart = { items: [] };
                 return db
