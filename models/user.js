f732c36b4d6426765e1d329d22f9b8e60bcc44c1
Thy Doan <doanthienthy91@gmail.com>
2022-04-13 21:42:51 +0700

    Lưu trữ nhiều sản phẩm trong giỏ hàng

--- models/user.js
+++ models/user.js
@@ -17,10 +17,19 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex(cp => {
-        //     return cp._id === product._id;
-        // })
-        const updatedCart = { items: [{ productId: new ObjectId(product._id), quantiy: 1 }] };
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        })
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantiy + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({ productId: new ObjectId(product._id), quantiy: 1 });
+        }
+        const updatedCart = { items: updatedCartItems };
         const db = getDb();
         return db
             .collection('users')
