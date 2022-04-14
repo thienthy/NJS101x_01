6068044730cf195b50ce9371819fb55d39413256
Thy Doan <doanthienthy91@gmail.com>
2022-04-14 20:54:19 +0700

    Xóa sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -88,7 +88,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DELETED PRODUCT!');
       res.redirect('/admin/products');
