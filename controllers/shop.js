0b190147a82e962a6faac6bbaaa5ee1237f789eb
Thy Doan <doanthienthy91@gmail.com>
2022-04-14 23:10:22 +0700

    Nhận & Hiển thị các Đơn đặt hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -103,14 +103,13 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
         orders: orders
-      });
+      })
     })
     .catch(err => console.log(err));
 };
