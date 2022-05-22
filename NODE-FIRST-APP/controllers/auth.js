c07d058d087f8ca124397cc347f6bf5d36358188
Thy Doan <doanthienthy91@gmail.com>
2022-05-22 09:10:41 +0700

    Triển khai Luồng xác thực

--- NODE-FIRST-APP/controllers/auth.js
+++ NODE-FIRST-APP/controllers/auth.js
@@ -8,8 +8,16 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('6288f41d6c65a945f8ce1c3c')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
@@ -17,6 +25,28 @@ exports.postLogin = (req, res, next) => {
         console.log(err);
         res.redirect('/');
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
     })
     .catch((err) => console.log(err));
 };
