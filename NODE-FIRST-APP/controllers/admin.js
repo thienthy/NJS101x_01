95d3a57e8740b55181279a94a060c705a31e691a
Thy Doan <doanthienthy91@gmail.com>
2022-05-23 22:31:03 +0700

    Sử dụng Middleware xử lý lỗi ở Express.js

--- NODE-FIRST-APP/controllers/admin.js
+++ NODE-FIRST-APP/controllers/admin.js
@@ -71,7 +71,10 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: 'Database operation failed, please try again',
       //   validationErrors: [],
       // });
-      res.redirect('/500');
+      // res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -96,7 +99,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: [],
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
