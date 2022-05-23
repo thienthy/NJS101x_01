e0bf23885d3624fc4da2918b896b23749acc6b64
Thy Doan <doanthienthy91@gmail.com>
2022-05-23 22:18:22 +0700

    Trả lại các trang lỗi

--- NODE-FIRST-APP/controllers/admin.js
+++ NODE-FIRST-APP/controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const { validationResult } = require('express-validator/check');
 
 const Product = require('../models/product');
@@ -25,7 +27,7 @@ exports.postAddProduct = (req, res, next) => {
     console.log(errors.array());
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/edit-product',
+      path: '/admin/add-product',
       editing: false,
       hasError: true,
       product: {
@@ -40,6 +42,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
+    _id: new mongoose.Types.ObjectId('62582e13c9704b2f58b8ab93'),
     title: title,
     price: price,
     description: description,
@@ -54,7 +57,21 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch((err) => {
-      console.log(err);
+      // return res.status(500).render('admin/edit-product', {
+      //   pageTitle: 'Add Product',
+      //   path: '/admin/add-product',
+      //   editing: false,
+      //   hasError: true,
+      //   product: {
+      //     title: title,
+      //     imageUrl: imageUrl,
+      //     price: price,
+      //     description: description,
+      //   },
+      //   errorMessage: 'Database operation failed, please try again',
+      //   validationErrors: [],
+      // });
+      res.redirect('/500');
     });
 };
 
