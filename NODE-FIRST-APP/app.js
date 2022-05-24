1e47c6d205a5fddc7b6a3e880e51003b7f7d6974
Thy Doan <doanthienthy91@gmail.com>
2022-05-24 21:25:31 +0700

    Lưu trữ dữ liệu tệp trong cơ sở dữ liệu

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -27,7 +27,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, new Date().toISOString().slice(0, 13) + '-' + file.originalname);
   },
 });
 
