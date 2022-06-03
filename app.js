374b4a80dcce43eff750d3e0e4c99bbfb48a6acf
Thy Doan <doanthienthy91@gmail.com>
2022-06-03 11:59:47 +0700

    fix

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use('/manageStaff', manageStaffRoutes);
 app.use(authRoutes);
 app.use(errorController.get404);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server at http://localhost:${PORT}`);
